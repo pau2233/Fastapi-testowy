7238f9621d5127b5d1eadea87750b4d756ea7aeb
pau2233 <paula126@op.pl>
2022-11-13 15:11:08 +0100

    req2 i main - pierwsze

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
+from typing import Union
+from primePy import primes
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "By sprawdzić czy liczba jest pierwsza - http://localhost:8000/prime/10619863  " +
+            " By odwrócić kolory obrazka  - http://127.0.0.1:8000/inversion/image.jpg"
+            }
 
+@app.get("/prime/{number}")
+async def isprimenum(number: int):
+    if isprimenum(number):  return {"message": "Liczba jest pierwsza"}
+    else:
+        return {"message": "Liczba nie jest pierwsza"}
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
